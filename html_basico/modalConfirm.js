033dda6874a74fc081cf89198777add15d987ff0
Leticia Sampaio <leticia.sampaio@prodigious.com>
2020-09-16 19:05:20 -0300

    Caminhos funcionando com js simples.

---
+++ html_basico/modalConfirm.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById("modalContainer");
+var save = document.getElementById("save");
+var exit = document.getElementById("close");
+
+save.onclick = function() {
+    modal.style.display = "block";
+}
+
+exit.onclick = function() {
+    modal.style.display = "none";
+    window.location.replace("home.html");
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        window.location.replace("home.html");
+    }
+}
